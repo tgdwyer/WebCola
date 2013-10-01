691d7642ecb3560be17d328e1a0b6e736d160ec0
Tim Dwyer <tgdwyer@gmail.com>
2013-10-01 15:21:28 +1000

    touch input on rectangle example

--- WebCola/rectangletest.js
+++ WebCola/rectangletest.js
@@ -7,6 +7,9 @@ var draw;
 var rs;
 var undoStack = [];
 var animate = false;
+var touchStart = window.navigator.msPointerEnabled ? "MSPointerDown" : "touchstart";
+var touchMove = window.navigator.msPointerEnabled ? "MSPointerMove" : "touchmove";
+var touchEnd = window.navigator.msPointerEnabled ? "MSPointerUp" : "touchend";
 function toggleAnimation() {
     animate = !animate;
 }
@@ -29,6 +32,9 @@ function init() {
         x = w.innerWidth || e.clientWidth || g.clientWidth,
         y = w.innerHeight || e.clientHeight || g.clientHeight;
     draw = SVG('canvas').size(canvaswidth = x - 30, canvasheight = y - 100);
+    var svg = draw.node;
+    svg.addEventListener(touchMove, onTouchMove);
+    svg.addEventListener(touchEnd, onTouchEnd);
 }
 
 function updateRectCount(n) {
@@ -50,16 +56,58 @@ function drawRects() {
             y: (canvasheight - h) * r(),
         };
         dims.push(d);
-        rs.push(draw.rect(d.w, d.h).move(d.x, d.y).attr('fill-opacity', 0.5).draggable());
+        var rect = draw.rect(d.w, d.h).move(d.x, d.y).attr('fill-opacity', 0.5);
+        if (touchStart !== "MSPointerDown") {
+            rect.draggable();
+            rect.dragmove = function (delta, event) {
+                removeOverlaps(rs);
+            };
+        }
+        rect.node.addEventListener(touchStart, onTouchStart);
+        rect.node.style.msTouchAction = "none";
+        rs.push(rect); 
     }
-    rs.forEach(function (r) {
-        r.dragmove = function (delta, event) {
-            removeOverlaps(rs);
-        };
-    });
     undoStack.push(dims);
     dump(rs);
     return rs;
+}
+
+var dragRect = null;
+
+function getTouchPoint(e, o) {
+    var m = o.getScreenCTM();
+    var p = draw.node.createSVGPoint();
+    p.x = e.clientX || e.targetTouches[0].clientX;
+    p.y = e.clientY || e.targetTouches[0].clientY;
+    return p.matrixTransform(m.inverse());
+}
+
+function onTouchStart(e) {
+    dragRect = e.srcElement;
+    var p = getTouchPoint(e, dragRect);
+    var rx = Number(dragRect.getAttribute("x"));
+    var ry = Number(dragRect.getAttribute("y"));
+    offset = { x: p.x - rx, y: p.y - ry };
+    dragRect.setAttribute("fill", "green");
+}
+
+function onTouchMove(e) {
+    if (dragRect != null) {
+        var re = dragRect;
+        var p = getTouchPoint(e, dragRect);
+        re.setAttribute("x", p.x - offset.x);
+        re.setAttribute("y", p.y - offset.y);
+        removeOverlaps(rs);
+        e.preventDefault();
+    }
+}
+
+function onTouchEnd(e) {
+    if (dragRect != null) {
+        var re = dragRect;
+        re.setAttribute("fill", "black");
+        dragRect = null;
+    }
 }
 
 function removeOverlaps(rects) {
