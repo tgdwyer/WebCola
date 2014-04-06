73a86aa7d797f886f8d764ee8b69218b4d5a6945
Nicholas Bollweg (Nick) <nick.bollweg@gmail.com>
2014-04-06 17:11:26 -0400

    moving stuff to be ready for more building stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,42 +1,55 @@
 module.exports = function (grunt) {
-  var _ = require("underscore"),
-    fs = require("fs");
+  var _ = require('underscore'),
+    fs = require('fs'),
+    path = require('path'),
+    
+    pkg = grunt.file.readJSON('package.json'),
+    bwr = grunt.file.readJSON('bower.json');
+
   require('load-grunt-tasks')(grunt);
   require('./tasks/examples')(grunt);
 
   function _build_examples(){
-    var examples = grunt.file.expand(["site/examples/*/index.jade"]);
+    var examples = grunt.file.expand(['site/examples/*/index.jade']);
     return examples.map(function(example){
       return {
         src: example,
-        dest: example.replace(/^site(.*)jade$/, "dist$1html")
+        dest: example.replace(/^site(.*)jade$/, 'dist$1html')
       }
     });
   }
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    bwr: grunt.file.readJSON('bower.json'),
+    pkg: pkg,
+    bwr: bwr,
     watch: {
       default: {
-        files: ["<%= concat.dist.src %>", "Gruntfile.js", "templates/*"],
-        tasks: ["default"]
+        files: ['<%= concat.dist.src %>', 'Gruntfile.js', 'templates/*'],
+        tasks: ['default']
       },
       typescript: {
-        files: ["<%= typescript.base.src %>"],
-        tasks: ["typescript"]
+        files: ['<%= typescript.base.src %>'],
+        tasks: ['typescript']
       },
       test: {
-        files: ["WebCola/test/*.js"],
-        tasks: ["qunit"]
+        files: ['WebCola/test/*.js'],
+        tasks: ['qunit']
+      },
+      site: {
+        files: ['site/data/**'],
+        tasks: ['newer:copy']
       },
       jade: {
-        files: ["site/**/*.jade"],
-        tasks: ["jade"]
+        files: ['site/**/*.jade', '!site/**/_*.jade'],
+        tasks: ['newer:jade']
+      },
+      jade_partials: {
+        files: ['site/**/_*.jade'],
+        tasks: ['jade']
       },
       less: {
-        files: ["site/**/*.less"],
-        tasks: ["less"]
+        files: ['site/**/*.less'],
+        tasks: ['newer:less']
       }
     },
     typescript: {
@@ -91,52 +104,73 @@ module.exports = function (grunt) {
       all: ['WebCola/test/*.html']
     },
     examples: {
-      all: ["site/examples/*/"]
+      all: ['site/examples/*/']
     },
     yuidoc: {
       compile: {
         name: 'cola.js',
         description: 'Javascript constraint based layout for high-quality graph visualization and exploration using D3.js and other web-based graphics libraries.',
         version: '1',
-        url: 'http://marvl.infotech.monash.edu/webcola',
+        url:pkg.homepage,
         options: {
-          paths: 'WebCola/src',
-          outdir: 'WebCola/doc'
+          paths: ['src', 'lib'],
+          outdir: 'dist/api'
         }
       }
     },
     jade: {
       site: {
         options: {
+          pretty: true,
           data: function(dest, src){
-            var root = dest.split("/")
+            var root = dest.split('/')
               .slice(2)
-              .map(function(){ return ".."; })
-              .join("/");
-            root = root ? root + "/" : root;
+              .map(function(){ return '..'; })
+              .join('/');
+            root = root ? root + '/' : root;
 
             return _.extend({},
               grunt.config.data,
               {
                 root: root,
-                bc: root + "bower_components/",
+                bc: root + 'bower_components/',
                 examples: grunt.file.expand(grunt.config.data.examples.all)
-                  .map(function(ex){ return ex.replace(/^site\//, ""); })
+                  .map(function(ex){ return ex.replace(/^site\//, ''); }),
+                inline: function(file_src){
+                  return fs.readFileSync(
+                    path.join(path.dirname(src[0]), file_src)
+                  );
+                }
               }
             );
           }
         },
-        files: [{src: ["site/index.jade"], dest: "dist/index.html"}]
+        files: [{src: ['site/index.jade'], dest: 'dist/index.html'}]
           .concat(_build_examples())
       }
     },
     less: {
       default: {
         files: [{
-          src: ["site/style.less"],
-          dest: "dist/style.css"
+          src: ['site/style.less'],
+          dest: 'dist/style.css'
         }]
       }
+    },
+    copy: {
+      data: {
+        files: [
+          {expand: true, cwd: 'site', src: ['data/**'], dest: 'dist'},
+        ]
+      }
+    },
+    concurrent: {
+      site: [
+        'newer:jade',
+        'newer:less',
+        'newer:copy',
+        'newer:yuidoc'
+      ]
     }
   });
 
@@ -157,11 +191,8 @@ module.exports = function (grunt) {
     'qunit'
   ]);
 
-  grunt.registerTask('docs', [
-    'jade',
-    'less',
-    'yuidoc',
-    'typescript:examples'
+  grunt.registerTask('site', [
+    'concurrent:site'
   ]);
 
   grunt.registerTask('full', [
---
+++ site/examples/alignment/alignment.js
@@ -0,0 +1,96 @@
+var width = 800,
+    height = 300;
+
+var color = d3.scale.category20();
+
+var d3cola = cola.d3adaptor()
+    .linkDistance(120)
+    .avoidOverlaps(true)
+    .size([width, height]);
+
+var svg = d3.select("#example").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+d3.json("../../data/alignmentconstraints.json", function (error, graph) {
+    graph.nodes.forEach(function (v) { v.x = 400, v.y = 70 });
+    d3cola
+        .nodes(graph.nodes)
+        .links(graph.links)
+        .constraints(graph.constraints)
+        .start(10,10,10);
+
+    var link = svg.selectAll(".link")
+        .data(graph.links)
+      .enter().append("line")
+        .attr("class", "link");
+
+    var guideline = svg.selectAll(".guideline")
+        .data(graph.constraints.filter(function (c) { return c.type === 'alignment' }))
+      .enter().append("line")
+        .attr("class", "guideline")
+        .attr("stroke-dasharray", "5,5");
+
+    var node = svg.selectAll(".node")
+        .data(graph.nodes)
+      .enter().append("rect")
+        .attr("class", "node")
+        .attr("width", function (d) { return d.width; })
+        .attr("height", function (d) { return d.height; })
+        .attr("rx", 5).attr("ry", 5)
+        .style("fill", function (d) { return color(1); })
+        .call(d3cola.drag);
+
+    var label = svg.selectAll(".label")
+        .data(graph.nodes)
+       .enter().append("text")
+        .attr("class", "label")
+        .text(function (d) { return d.name; })
+        .call(d3cola.drag);
+
+    node.append("title")
+        .text(function (d) { return d.name; });
+
+    d3cola.on("tick", function () {
+        link.attr("x1", function (d) { return d.source.x; })
+            .attr("y1", function (d) { return d.source.y; })
+            .attr("x2", function (d) { return d.target.x; })
+            .attr("y2", function (d) { return d.target.y; });
+
+        guideline
+            .attr("x1", function (d) { return getAlignmentBounds(graph.nodes, d).x; })
+            .attr("y1", function (d) {
+                return d.bounds.y;
+            })
+            .attr("x2", function (d) { return d.bounds.X; })
+            .attr("y2", function (d) {
+                return d.bounds.Y;
+            });
+
+        node.attr("x", function (d) { return d.x - d.width / 2; })
+            .attr("y", function (d) { return d.y - d.height / 2; });
+
+        label.attr("x", function (d) { return d.x; })
+             .attr("y", function (d) {
+                 var h = this.getBBox().height;
+                 return d.y + h/4;
+             });
+    });
+});
+
+function getAlignmentBounds(vs, c) {
+    var os = c.offsets;
+    if (c.axis === 'x') {
+        var x = vs[os[0].node].x;
+        c.bounds = new cola.vpsc.Rectangle(x, x, 
+            Math.min.apply(Math, os.map(function (o) { return vs[o.node].bounds.y - 20; })),
+            Math.max.apply(Math, os.map(function (o) { return vs[o.node].bounds.Y + 20; })));
+    } else {
+        var y = vs[os[0].node].y;
+        c.bounds = new cola.vpsc.Rectangle(
+            Math.min.apply(Math, os.map(function (o) { return vs[o.node].bounds.x - 20; })),
+            Math.max.apply(Math, os.map(function (o) { return vs[o.node].bounds.X + 20; })),
+            y, y);
+    }
+    return c.bounds;
+}
