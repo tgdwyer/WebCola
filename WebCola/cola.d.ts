dbf04fb15ead2a451deadafb073dadeb0214c0b4
Tim Dwyer <tgdwyer@gmail.com>
2015-09-23 16:52:42 -0700

    .d.ts

--- WebCola/cola.d.ts
+++ WebCola/cola.d.ts
@@ -725,6 +725,7 @@ declare module cola {
         source: NodeType;
         target: NodeType;
         length?: number;
+        weight?: number;
     }
     /**
      * Main interface to cola layout.
