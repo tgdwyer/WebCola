f242694bb7139206587622ecb370f1b565f5bf28
Tim Dwyer <tgdwyer@gmail.com>
2018-02-10 14:30:59 +1100

    matrix test

--- WebCola/test/matrixperftest.ts
+++ WebCola/test/matrixperftest.ts
@@ -1,73 +1,79 @@
 import * as QUnit from 'qunit';
+
+QUnit.module("matrix performance");
 QUnit.test("matrix perf experiment", function (assert) {
     console.log("Array test:");
     const repeats = 1000, n = 100;
-    let startTime: number, M: number[][], description: string;
-    const startTimer = (desc: string) => {
+    let startTime: number, M: number[][], MT: Float32Array[], description: string;
+    const time = (desc:string, func: ()=>void) => {
         description = desc;
         startTime = window.performance.now();
-    }
-    const stopTimer = () => {
+        func();
         const t = window.performance.now() - startTime
         console.log(`${description} = ${t}`)
         return t;
-    } 
-
-    startTimer("init matrix");
-    for (var k = 0; k < repeats; ++k) {
-        M = new Array(n);
-        for (var i = 0; i < n; ++i) {
-            M[i] = new Array(n);
-        }
     }
-    let totalRegularArrayTime = stopTimer();
 
-    startTimer("write array");
-    for (var k = 0; k < repeats; ++k) {
-        for (var i = 0; i < n; ++i) {
-            for (var j = 0; j < n; ++j) {
-                M[i][j] = 1;
+    let totalRegularArrayTime = time("init matrix", ()=> {
+        for (var k = 0; k < repeats; ++k) {
+            M = new Array(n);
+            for (var i = 0; i < n; ++i) {
+                M[i] = new Array(n);
             }
         }
-    }
-    totalRegularArrayTime += stopTimer();
+    });
 
-    startTimer("read array");
-    for (var k = 0; k < repeats; ++k) {
-        var sum = 0;
-        for (var i = 0; i < n; ++i) {
-            for (var j = 0; j < n; ++j) {
-                sum += M[i][j];
+    totalRegularArrayTime += time("write array",() => {
+        for (var k = 0; k < repeats; ++k) {
+            for (var i = 0; i < n; ++i) {
+                for (var j = 0; j < n; ++j) {
+                    M[i][j] = 1;
+                }
             }
         }
-        //equal(sum, n * n);
-    }
-    totalRegularArrayTime += stopTimer();
+    });
 
+    totalRegularArrayTime += time("read array", () => {
+        for (var k = 0; k < repeats; ++k) {
+            var sum = 0;
+            for (var i = 0; i < n; ++i) {
+                for (var j = 0; j < n; ++j) {
+                    sum += M[i][j];
+                }
+            }
+            assert.equal(sum, n * n);
+        }
+    });
+    
     console.log("Typed Array test:");
-    startTimer("init");
-    var MT;
-    for (var k = 0; k < repeats; ++k) {
-        MT = new Float32Array(n * n);
-    }
-    let totalTypedArrayTime = stopTimer();
-    
-    startTimer("write array");
-    for (var k = 0; k < repeats; ++k) {
-        for (var i = 0; i < n * n; ++i) {
-            MT[i] = 1;
+    let totalTypedArrayTime = time("init", () => {
+        MT = new Array<Float32Array>(n);
+        for (var k = 0; k < repeats; ++k) {
+            for (var i = 0; i < n; ++i) {
+                MT[i] = new Float32Array(n);
+            }
         }
-    }
-    totalTypedArrayTime += stopTimer();
+    });
     
-    startTimer("read array");
+    totalTypedArrayTime += time("write array", () => {
     for (var k = 0; k < repeats; ++k) {
-        var sum = 0;
-        for (var i = 0; i < n * n; ++i) {
-            sum += MT[i];
+        for (var i = 0; i < n; ++i) {
+            for (let j = 0; j < n; ++j) {
+                MT[i][j] = 1;
+            }
         }
-        //equal(sum, n * n);
-    }
-    totalTypedArrayTime += stopTimer();
-    assert.ok(isNaN(totalRegularArrayTime) || totalRegularArrayTime < totalTypedArrayTime, "totalRegularArrayTime=" + totalRegularArrayTime + " totalTypedArrayTime="+totalTypedArrayTime+" - if this consistently fails then maybe we should switch to typed arrays");
+    }});
+    
+    totalTypedArrayTime += time("read array", () => {
+        for (var k = 0; k < repeats; ++k) {
+            var sum = 0;
+            for (var i = 0; i < n; ++i) {
+                for (let j = 0; j < n; ++j) {
+                    sum += MT[i][j];
+                }
+            }
+            assert.equal(sum, n*n);
+        }
+    });
+    assert.ok(totalRegularArrayTime < totalTypedArrayTime, "totalRegularArrayTime=" + totalRegularArrayTime + " totalTypedArrayTime="+totalTypedArrayTime+" - if this consistently fails then maybe we should switch to typed arrays");
 });
